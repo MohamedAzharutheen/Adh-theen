be5e3722e13e368fb232dd046b47d6dd290e98ff
azhar <azhar@opseazy.org>
2024-10-26 20:09:43 +0530

    #

--- pages/guidance/hajj-guide.js
+++ pages/guidance/hajj-guide.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { Chrono } from "react-chrono";
+import dynamic from "next/dynamic";
 import Layout from "@/component/layout";
 
-// const Chrono = dynamic(() => import("react-chrono").then((mod) => mod.Chrono), {
-//   ssr: false,
-// });
+const Chrono = dynamic(() => import("react-chrono").then((mod) => mod.Chrono), {
+  ssr: false,
+});
 
 const ZigzagTimeline = () => {
   const items = [
@@ -185,6 +185,137 @@ const ZigzagTimeline = () => {
     width: 97%;
     padding: 0.5rem 0 0.5rem 0.5rem;
     }
+     .diSIwa.active {
+    background: #f5f5f5;
+    color: #E9C751 !important;
+	border-radius: 10px;
+    border: 1px solid !important;
+    font-size: 4rem !important;
+	padding: 5px !important;
+}
+.diSIwa {
+    background: #f5f5f5;
+    color: #E9C751 !important;
+	border-radius: 10px;
+    border: 1px solid !important;
+    font-size: 4rem !important;
+	padding: 5px !important;
+
+}
+  .jSOuST {
+    color: #000000a8;
+    font-size: 12px !important;
+    font-weight: 400 !important;
+	line-height: 30px ;
+    margin: 0;
+    text-align: left;
+    width: 97%;
+    padding: 0.5rem 0 0.5rem 0.5rem;
+    }
+	.hDXPPT {
+		align-items: flex-start;
+		background: #ffffff;
+		border-radius: 8px;
+		display: flex;
+		position: absolute;
+		filter: drop-shadow(0 0 1px rgba(0, 0, 0, 0.3));
+		flex-direction: column;
+		justify-content: flex-start;
+		line-height: 1.5em;
+		max-width: 100% !important;
+		min-height: 200px;
+		position: relative;
+		text-align: left;
+		width: 98%;
+		z-index: 0;
+	}
+	.hxLLRt {
+		display: none !important;
+	}
+	@media only screen and (max-width: 768px){
+		.jSOuST {
+			color: #000;
+			font-size: 14px !important;
+			font-weight: 400 !important;
+			line-height: 25px !important;
+			margin: 0;
+			text-align: left;
+			width: 97%;
+			padding: 0.5rem 0 0.5rem 0.5rem;
+			}   
+
+	}
+
+	.bLHzlm {
+		color: #006C35 !important;
+		font-size: 2rem !important;
+		
+	}
+	.bZVtmZ:not(.using-icon).active {
+		background: #006C35 !important;
+		border: 3px solid #006C35 !important;
+	}
+	.bZVtmZ:not(.using-icon) {
+		background: #006C35 !important;
+	}
+	.bZVtmz {
+     width: 4rem !important;
+	 height: 4rem !important;
+	}
+	.hJHChW::before {
+		background: #006C35 !important;
+		width: 3px;
+		height: 2rem;
+		position: absolute;
+		content: '';
+		display: block;
+		left: 50%;
+		top: -1rem;
+		transform: translateY(-50%) translateX(-50%);
+	}
+	.hJHChW::after {
+		background: #006C35 !important;
+		content: '';
+		display: block;
+		height: 100%;
+		left: 50%;
+		position: absolute;
+		width: 3px;
+		z-index: 0;
+		transform: translateX(-50%);
+	}
+	.hFrYSD {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 1.5rem;
+		height: 1.5rem;
+		position: absolute;
+		top: calc(0%) !important;
+		background: #EACA5A !important;
+		transform: translateY(-50%) rotate(225deg);
+		z-index: -1;
+		right: -13px;
+	}
+
+	.hDXPPT {
+		align-items: flex-start;
+		/* background: #EACA5A; */
+		border-radius: 8px  !important;
+		display: flex;
+		position: absolute;
+		filter: drop-shadow(0 0 1px rgba(0, 0, 0, 0.3));
+		flex-direction: column;
+		justify-content: flex-start;
+		line-height: 1.5em;
+		max-width: 300px;
+		border: 1px solid #EACA5A !important;
+		min-height: 200px;
+		position: relative;
+		text-align: left;
+		width: 98%;
+		z-index: 0;
+	}
     `}
 </style>
 </>
